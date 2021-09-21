3628cda703ffe96cecea11a6425c4d4f34dbfa44
Grant <grantgariepy@protonmail.com>
2021-09-21 11:05:42 -0600

    cleaned some stuff up

--- src/pages/AppFunction.js
+++ src/pages/AppFunction.js
@@ -59,25 +59,25 @@ function App() {
       const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase() ))
 
       return <div className="coin-app">
-      <div className="coin-search">
-        <h1 className="coin-text">Search</h1>
-        <form>
-          <input type="text" className="coin-input" placeholder="Search" onChange={handleChange}/>
-        </form>
-      </div> 
-      {filteredCoins.map(coin => {
-        return <Coin 
-          id={coin.id} 
-          name={coin.name} 
-          image={coin.image}
-          symbol={coin.symbol}
-          volume={coin.total_volume}
-          price={coin.current_price}
-          priceChange={coin.price_change_percentage_24h}
-          marketcap={coin.market_cap}
-          />
-      })}
-    </div>;
+              <div className="coin-search">
+                <h1 className="coin-text">Search</h1>
+                <form>
+                  <input type="text" className="coin-input" placeholder="Search" onChange={handleChange}/>
+                </form>
+              </div> 
+              {filteredCoins.map(coin => {
+                return <Coin 
+                  id={coin.id} 
+                  name={coin.name} 
+                  image={coin.image}
+                  symbol={coin.symbol}
+                  volume={coin.total_volume}
+                  price={coin.current_price}
+                  priceChange={coin.price_change_percentage_24h}
+                  marketcap={coin.market_cap}
+                  />
+              })}
+            </div>;
     }
     
     function About() {
